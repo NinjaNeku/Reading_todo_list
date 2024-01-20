8e93df9fa0136d8719c42a2d7295f715303ce383
Aayush <aayushgautam2502@gmail.com>
2024-01-20 16:49:24 +0530

    fix: typo

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App(){
     }, []);
 
     const deleteBookById = async(id) => {
-        await axios.delete(`http//localhost:3001/books/${id}`);
+        await axios.delete(`http://localhost:3001/books/${id}`);
 
         const updatedBooks = books.filter((book) => {
             return book.id !== id;
