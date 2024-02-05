aa06b7bf72f24ff5d6677b4bbe0aec264dce3c4c
Aayush <aayushgautam2502@gmail.com>
2024-02-05 23:01:35 +0530

    Refactor: integrate use of context

--- src/App.js
+++ src/App.js
@@ -1,58 +1,22 @@
-import { useState, useEffect } from "react";
-import React from "react";
+import { useEffect, useContext } from 'react';
 import BookCreate from './components/BookCreate';
 import BookList from './components/BookList';
-import axios from 'axios';
+import BooksContext from './context/books';
 
+function App() {
+  const { fetchBooks } = useContext(BooksContext);
 
-function App(){
-    const [books, setBooks] = useState([]);
+  useEffect(() => {
+    fetchBooks();
+  }, [fetchBooks]);
 
-    const fetchBooks = async() => {
-        const response = await axios.get('https://localhost:3001/books');
-        setBooks(response.data);
-    }
-
-    useEffect(() => {
-        fetchBooks();
-    }, []);
-
-    const deleteBookById = async(id) => {
-        await axios.delete(`http://localhost:3001/books/${id}`);
-
-        const updatedBooks = books.filter((book) => {
-            return book.id !== id;
-        })
-        setBooks(updatedBooks);
-    }
-
-    const createBook = async(title) => {
-        const response = await axios.post('http://localhost:3001/books',{title});
-        const updatedBooks = [...books, response.data,]
-        setBooks(updatedBooks);
-    };
-
-    const editBookById = async(id, newTitle) => {
-        const response = await axios.put(`http://localhost:3001/books/${id}`, {
-            title: newTitle
-        })
-
-        const updatedBooks = books.map((book) => {
-            if(book.id === id){
-                return {...book, ...response.data}
-            }
-            return book;
-        })
-        setBooks(updatedBooks);
-    }
-
-    return (
+  return (
     <div className="app">
-        <h1>Reading List</h1>
-        <BookList books = {books}  onEdit={editBookById} onDelete = {deleteBookById} />
-        <BookCreate onCreate={createBook} />
+      <h1>Reading List</h1>
+      <BookList />
+      <BookCreate />
     </div>
-    )
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import './index.css';
 import React, { createElement } from "react";
 import reactDOM from 'react-dom/client';
 import App from './App';
+import { Provider } from './context/books';
 
 const el = document.getElementById('root');
 const root = reactDOM.createRoot(el);
 
-root.render(<App />);
+root.render(<Provider><App /></Provider>);
